cad3e6ee64ea37a9eb592c05a90cd45889fca468
Wick McLean <wickmclean@gmail.com>
2018-11-28 12:38:39 -0500

    Add files via upload
    A problem from Project Euler:
    
    "Each new term in the Fibonacci sequence is generated by adding the previous two terms. By starting with 1 and 2, the first 10 terms will be:
    
    1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
    
    By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms."

---
+++ Even_fibonacci_sum.py
@@ -0,0 +1,24 @@
+'''
+Created on Nov 27, 2018
+
+@author: wickm
+'''
+#"Find the sum of the even valued Fibonacci numbers from \
+# 1 to four million"
+
+def fibonacci():
+    sumlist=[]
+    list=(1,1)
+    for nextnumber in range(0,4000000):
+        while list[1] <= nextnumber:
+            fibo=list[0]+list[1]
+            list=(list[1],fibo)
+            if fibo % 2 == 0:
+                sumlist.append(fibo)
+    print('The sum of the numbers') 
+    print(sumlist)
+    print('Is: ')
+    print(sum(sumlist))
+
+
+fibonacci()
